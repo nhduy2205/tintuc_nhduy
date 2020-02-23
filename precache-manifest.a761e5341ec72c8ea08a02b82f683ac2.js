e8633fe38b3816141cdc07dc9158a12ac6985d0b
nhduy <nhduy220597@gmail.com>
2020-02-23 21:35:00 +0700

    first commit

---
+++ precache-manifest.a761e5341ec72c8ea08a02b82f683ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3963d4eaa623988541602921a2eaa6d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc0fd38c68c42958c17",
+    "url": "/static/css/main.a4bd1e0f.chunk.css"
+  },
+  {
+    "revision": "965243fa20580ae99bd2",
+    "url": "/static/js/2.89aeebed.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.89aeebed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbc0fd38c68c42958c17",
+    "url": "/static/js/main.c52a539f.chunk.js"
+  },
+  {
+    "revision": "b30322780dd2547012a8",
+    "url": "/static/js/runtime-main.29daf5ff.js"
+  }
+]);
